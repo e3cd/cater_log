e9f0c11e2fbcf524788e1ad9577206dba0fd226e
Allan Gao <allangao95@gmail.com>
2018-11-02 11:42:12 +1100

    show all party caterers on party page

--- app/controllers/event_types_controller.rb
+++ app/controllers/event_types_controller.rb
@@ -3,27 +3,27 @@ class EventTypesController < ApplicationController
   end
 
   def party
-    @party = CaterInformation.where(type_of_event: 0)
+    @party = CatererInformation.where(type_of_event: 0)
   end
 
   def wedding
-    @wedding = CaterInformation.where(type_of_event: 1)
+    @wedding = CatererInformation.where(type_of_event: 1)
   end
 
   def conference
-    @conference = CaterInformation.where(type_of_event: 2)
+    @conference = CatererInformation.where(type_of_event: 2)
   end
 
   def formal
-    @formal= CaterInformation.where(type_of_event: 3)
+    @formal= CatererInformation.where(type_of_event: 3)
   end
 
   def cocktail
-    @cocktail= CaterInformation.where(type_of_event: 4)
+    @cocktail= CatererInformation.where(type_of_event: 4)
   end
 
   def concert
-    @concert = CaterInformation.where(type_of_event: 5)
+    @concert = CatererInformation.where(type_of_event: 5)
   end
 
 end
