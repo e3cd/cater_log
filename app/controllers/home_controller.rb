8fddbb86213e3affa3938886cb2903bc39179a04
Veomaany <veomany.khammarath@gmail.com>
2018-11-02 16:29:05 +1100

    add content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
-    def index 
+    def index
+        @caterer_information = CatererInformation.all
+        @caterer_information = @caterer_information.sample(3)
     end
 end
