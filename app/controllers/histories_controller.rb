132ef4b3da7b32cf28c0b70609e8a2964288dc68
Hamish Lees <hamishlees6@gmail.com>
2018-11-07 00:18:45 +1100

    clean up history controller and fix up error message issue

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,6 +1,7 @@
 class HistoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_history, only: [:show, :edit, :update, :destroy]
+  before_action :set_caterer_menu_id, only: [:new, :create]
 
   #Show all histories for the unique user
   def index
@@ -27,37 +28,21 @@ class HistoriesController < ApplicationController
   def show
   end
 
-  # GET /histories/new
   def new
     @history = History.new
-    @caterer_menu_id = params[:id]
-    @caterer_menu = CatererMenu.find(@caterer_menu_id)
-    @caterer_name = CatererInformation.find_by(user_id: @caterer_menu.user_id)[:business_name]
-    @menu = @caterer_menu[:description]
-    @cost = @caterer_menu[:price]
   end
 
   #Add new booking to database, then redirect to stripe payment
   def create
-    #Calculating the total_price of the booking
-    @caterer_menu_id = params[:history][:caterer_menu_id]
-    @caterer_menu = CatererMenu.find(@caterer_menu_id)
-    @cost = @caterer_menu[:price]
     @total_price = @cost * (params[:history][:number_of_heads]).to_f #* 100 #times 100 as Stripe deals in cents
-
-    #Saving History to the database
     @history = History.new(history_params)
     @history.price = @total_price
     @history.user_id = current_user.id
     @history.save
-
-    respond_to do |format|
-      if @history.save
-        format.html { redirect_to confirm_booking_path, notice: 'Please confirm details.' }
-      else
-        ########### BUG!!! NEEDS ERROR MESSAGES!!!!!! ###############
-        format.html { redirect_to new_booking_path id: 4 }
-      end
+    if @history.save
+      redirect_to confirm_booking_path, notice: 'Please confirm details.'
+    else
+      render action: "new", assigns: {id: @caterer_menu_id}
     end
   end
 
@@ -70,7 +55,7 @@ class HistoriesController < ApplicationController
     end
   end
 
-
+  ######### STILL AN ISSUE WITH LAST HERE...HOPEFULLY BE ABLE TO SELECT THE PROPER ONE, SO THEN SOMEONE CAN COME BACK AND PAY ########
   def confirm
     @last = History.last
     @caterer = CatererMenu.find(@last[:caterer_menu_id])
@@ -89,5 +74,14 @@ class HistoriesController < ApplicationController
 
   def history_params
     params.require(:history).permit(:first_name, :email, :booking_date, :number_of_heads, :caterer_menu_id, :caterer_name)
+  end
+
+  def set_caterer_menu_id
+    @caterer_menu_id ||= params[:id]
+    @caterer_menu_id ||= params[:history][:caterer_menu_id]
+    @caterer_menu = CatererMenu.find(@caterer_menu_id)
+    @caterer_name = CatererInformation.find_by(user_id: @caterer_menu.user_id)[:business_name]
+    @menu = @caterer_menu[:description]
+    @cost = @caterer_menu[:price]
   end
 end
